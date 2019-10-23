ab2d5e62b52919eaf092c7cd1c358a2ef47dad3e
Abdul Omar <aahmed@ucsd.edu>
2019-10-23 12:14:51 -0700

    added headers

--- src/DictionaryTrie/DictionaryTrie.cpp
+++ src/DictionaryTrie/DictionaryTrie.cpp
@@ -1,7 +1,10 @@
-/**
- * TODO: File Header
+/*  
+ * Filename: DictionaryTrie.cpp
+ * Author: Abdulkhaliq Omar/Andrew Master
+ * UserId: cs30s2019ac
+ * Date:    10/23/2019
+ * Sources of Help: None
  *
- * Author: Andrew Masters, Abdulkhaliq Omar
  */
 #include "DictionaryTrie.hpp"
 #include <iostream>
@@ -10,16 +13,21 @@
 #include <utility>
 #include <algorithm>
 
-
-    TrieNode* DictionaryTrie::getPrefixRoot(TrieNode* root, string prefix, int charIndex)const{
-
+/*
+ * Function Name: getPrefix()
+ * Function Prototype:TrieNode* DictionaryTrie::getPrefixRoot(TrieNode* root, string prefix, int charIndex)
+ * Description: this function returns the root of the subtrie under prefix
+ * Return Value: last node under prefix
+ */
+TrieNode* DictionaryTrie::getPrefixRoot(TrieNode* root, string prefix, int charIndex)const{
+    //base case
   	if( !root){
   		return nullptr;
   	}
-        //if less than current node go left
+    //if less than current node go left
   	if( prefix.at(charIndex) < root->letter) 
   		return getPrefixRoot( root->left, prefix, charIndex);
-  		//if greater than current node gor right
+  	//if greater than current node gor right
   	else if( prefix.at(charIndex) > root->letter)
   		return getPrefixRoot( root->right, prefix, charIndex);
   	//if equal but we have more letter left in word traverse middle child 
@@ -32,30 +40,54 @@
 
   	}
 
-    bool DictionaryTrie::insertHelper(string word, unsigned int freq, TrieNode ** node, int i){
-	if(*node == nullptr){
+
+/*
+ * Function Name: insertHelper()
+ * Function Prototype:bool DictionaryTrie::insertHelper(string word, unsigned int freq, TrieNode ** node, int i)
+ * Description: this function iserts a word in the TST
+ * Return Value: true if successfully inserted, false otherwise
+ */
+bool DictionaryTrie::insertHelper(string word, unsigned int freq, TrieNode ** node, int i){
+	
+  if(*node == nullptr){
 	    (*node) = new TrieNode(word[i]);
 	}
 	
+  //if char is less than current node char
 	if(word[i] < (*node)->letter){
 	    insertHelper(word, freq, &((*node)->left), i);
-	} else if(word[i] > (*node)->letter){
+	} 
+  //if char is greater than the current node char
+  else if(word[i] > (*node)->letter){
 	    insertHelper(word, freq, &((*node)->right), i);
-	} else {
+	} 
+  //if equal
+  else {
+      //if last letter of string
 	    if(i == word.size() ){
-
-		if((*node)->wordEnd) return false;
-		(*node)->freq = freq;
-		(*node)->wordEnd = true;
-		return true;
-	    } else {
+        //word already there
+		    if((*node)->wordEnd) return false;
+		     //otherwise
+        (*node)->freq = freq;
+		    (*node)->wordEnd = true;
+		     return true;
+	    } 
+      //if char is not last letter of word
+      else {
 	    	insertHelper(word, freq, &((*node)->middle), i+1);
 	    }
-	}
-	return false;	
-    }
+	 }
 
-    void DictionaryTrie::deleteAll(TrieNode* node){
+	 return false;	
+}
+
+/*
+ * Function Name: deleteAll()
+ * Function Prototype:void DictionaryTrie::deleteAll(TrieNode* node)
+ * Description: this function deletes the tree
+ * Return Value: NONE
+ */
+void DictionaryTrie::deleteAll(TrieNode* node){
 	
 	if(node == nullptr){
 	    return;
@@ -68,53 +100,97 @@
 	delete node;
     }
 
-    /* TODO: */
-    DictionaryTrie::DictionaryTrie(){
+    /*
+ * Function Name: DictionaryTrie()
+ * Function Prototype: DictionaryTrie();
+ * Description: constructor forclass
+ * Return Value: NONE
+ */
+DictionaryTrie::DictionaryTrie(){
 	root = nullptr;
-    }
-
-    /* TODO: */
-    bool DictionaryTrie::insert(string word, unsigned int freq){
+}
+/*
+ * Function Name: insert()
+ * Function Prototype:bool DictionaryTrie::bool DictionaryTrie::insert(string word, unsigned int freq)
+ * Description: this function iserts a word in the TST
+ *Parameters:
+            word- word to insert
+            freq - frequency of word
+ * Return Value: true if successfully inserted, false otherwise
+ */
+bool DictionaryTrie::insert(string word, unsigned int freq){
 
 	if(word.empty()) return false;
-	return insertHelper(word, freq, &root, 0);
-    }
-
-    /* TODO: */
-    bool DictionaryTrie::find(string word) const{
-
-      return getPrefixRoot(root, word, 0) != nullptr;
-    }
-
-    /* TODO: */
-    vector<string> DictionaryTrie::predictCompletions(string prefix,
-                                     unsigned int numCompletions){
-      vector<pair<string, unsigned int>> topCompletions;//number of completions
 	
-     vector<pair<string, unsigned int>> completions;//number of completions
-     
-     vector<string> vec;
+  return insertHelper(word, freq, &root, 0);
+}
+/*
+ * Function Name: find()
+ * Function Prototype: bool DictionaryTrie::find(string word) const
+ * Description: searches a word in the TST
+ *Parameters:
+            word- word to search
+            
+ * Return Value: true if successfully found, false otherwise
+ */
+bool DictionaryTrie::find(string word) const{
+  //check if word was found
+  return getPrefixRoot(root, word, 0) != nullptr;
+}
+/*
+* Function Name: predictCompletions()
+* Function Prototype: vector<string> DictionaryTrie::predictCompletions(string prefix,
+                                     unsigned int numCompletions)
+* Description: Return a vector holding up to numCompletions of the most frequent completions of prefix, 
+                and the completions must be valid words in the dictionary. 
+                These completions should be listed from most to least frequent.
 
-     if(!root) return vec;  	
+                If there are fewer than numCompletions legal completions, 
+                this method should return a vector with as many completions as possible.
+
+                If no completions exist, then the function should return a vector of size 0.
+
+                if there are more than one word/phrase with the same frequency, 
+                they should be ordered alphabetically. For example, if prefix is “a” and numCompletions is 1,
+                then if “ant” and “and” have the same largest frequency, only “and” should be returned.
+
+                the prefix itself may be included in results if the prefix itself is a valid word
+                in the DictionaryTrie and is among the numCompletions top most frequent completions
+*Parameters:
+            prefix - the prefix whose completions to search for
+            numCompletions - the number of search results
+            
+* Return Value: true if successfully found, false otherwise
+*/
+vector<string> DictionaryTrie::predictCompletions(string prefix,
+                                     unsigned int numCompletions){
+    
+    vector<pair<string, unsigned int>> topCompletions;//number of completions
+	
+    vector<pair<string, unsigned int>> completions;//number of completions
      
+    vector<string> vec;//the string of prefix completions to return
+
     
-     //get root of prefix subtrie
-     TrieNode* node = getPrefixRoot(root, prefix, 0);
+    if(!root) return vec; //empty tree	
+        
+    //get root of prefix subtrie
+    TrieNode* node = getPrefixRoot(root, prefix, 0);
     
-     if(!node) return vec;     
+    if(!node) return vec; //prefix does not exist    
     
     //if prefix is valid word itself
-     if(node && node->wordEnd) {    
-       
+    if(node && node->wordEnd) {    
+       //add to the completions
        completions.emplace_back(make_pair(prefix, node->freq));
-     }
+    }
+    //recursively get all the words in the subtree
+    getAllWords( node->middle, prefix, completions);
     
-     getAllWords( node->middle, prefix, completions);
+    //no valid completions
+    if(completions.size() < 1) return vec;
     
-     //no valid completions
-     if(completions.size() < 1) return vec;
-    
-     //sort the vectors in order of increasing frequency and alphabetically if tied	
+    //sort the vectors in order of increasing frequency and alphabetically if tied	
     sort(completions.begin(), completions.end(), 
        [](const pair<string,unsigned int> &left, const std::pair<string ,unsigned int> &right) {
        if(left.second != right.second) {
@@ -133,19 +209,19 @@
     //get the top completions
     for( int i = 0 ; i < numCompletions; i++) {  
      
-         topCompletions.emplace_back(make_pair(completions[i].first.substr(0, completions[i].first.length() -1), completions[i].second));      
+      topCompletions.emplace_back(make_pair(completions[i].first.substr(0, completions[i].first.length() -1), 
+                                  completions[i].second));      
    }
-   
-
-    for( int i = 0; i < topCompletions.size(); i++) { 
+   //return the top completions
+   for( int i = 0; i < topCompletions.size(); i++) { 
     
-      vec.emplace_back(topCompletions[i].first);
-    
-    }
-    return vec;
- }
+     vec.emplace_back(topCompletions[i].first);    
+   }
+  
+   return vec;
+}
 
- void DictionaryTrie::getAllWords(TrieNode* root, string prefix, vector<pair<string, unsigned int>>& vect) { 
+void DictionaryTrie::getAllWords(TrieNode* root, string prefix, vector<pair<string, unsigned int>>& vect) { 
    
    if(root != nullptr) {
 
@@ -168,13 +244,30 @@
    
  
 }
-/* TODO */
+/*
+ * Function Name: predictUnderscores()
+ * Function Prototype: vector<string> DictionaryTrie::predictUnderscores(
+    string pattern, unsigned int numCompletions)
+ * Description: searches for wildcard completions in TST
+ *Parameters:
+            wild card pattern to search for
+            
+ * Return Value: true if successfully found, false otherwise
+ */
 std::vector<string> DictionaryTrie::predictUnderscores(
     string pattern, unsigned int numCompletions) {
     return {};
 }
 
-/* TODO */
+/*
+ * Function Name: DictionaryTrie()
+ * Function Prototype: DictionaryTrie::~DictionaryTrie()
+ * Description: destructor for the class
+ *Parameters:
+            NONE
+            
+ * Return Value: true if successfully found, false otherwise
+ */
 DictionaryTrie::~DictionaryTrie() {
 	deleteAll(root);
 }
